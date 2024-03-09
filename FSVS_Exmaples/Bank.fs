310ad322ddeac40e2071e5220b642fcff915905c
Kate A Ibom <72076413+KateTurton@users.noreply.github.com>
2024-03-09 18:39:08 +0000

    Defined a record type called account in the bank module with two fields.

---
+++ FSVS_Exmaples/Bank.fs
@@ -0,0 +1,13 @@
+﻿module Bank
+
+open System
+// Defined a record type called Account
+// The record type has two fields: Name and Balance
+type Account = 
+    {
+      Name: string
+      Balance: float
+    }
+
+
+
