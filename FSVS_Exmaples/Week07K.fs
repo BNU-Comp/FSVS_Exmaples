70f3bc5ad671b1256c40671ad1f6d7d7455ce116
Kate A Ibom <72076413+KateTurton@users.noreply.github.com>
2024-03-15 09:13:37 +0000

    renamed file to week07K

---
+++ FSVS_Exmaples/Week07K.fs
@@ -0,0 +1,20 @@
+﻿module Week07K
+
+open System
+
+// Create a Customer type that has two fields: Name and Email address
+type Customer = 
+    {
+        Name: string
+        Email: string
+    }
+// add a method to print the customer details
+    member this.Print =
+        Console.WriteLine($"  {this.Name} {this.Email}")
+// add a method to update the customer email
+    member this.UpdateEmail newEmail =
+        {this with Email = newEmail}
+// create a customer called Kate with an email address
+let kate = {Name="Kate"; Email="Kate@gmail.com"}
+// print the customer details
+kate.Print
